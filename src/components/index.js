2a37d63171e9b905672ea25865aaa4e3a96983ba
Arigus Wahyu <ariguswahyu.np@gmail.com>
2022-10-28 17:41:53 +0700

    downgrade to react 17.0.2

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from './atoms';
+export * from './molecules';
+export * from './pages';
+export * from './organisms';
+export * from './tamplates';
---
+++ src/components/organisms/Page/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+// import { styled } from '@mui/material/styles';
+
+
+
+
+const Page = () => {
+    return (
+
+        <div>tes pagess</div>
+
+    )
+}
+
+// const RootStyle = styled(Page)({
+//     height: '100%'
+// });
+
+// const ContentStyle = styled('div')(({ theme }) => ({
+//     overflow: 'hidden',
+//     position: 'relative',
+//     backgroundColor: theme.palette.background.default
+// }));
+
+
+export default Page
+
+// import React from 'react'
+// import { alpha, styled } from '@mui/material/styles';
+
+
+// const RootStyle = styled(Page)({
+//     height: '100%'
+// });
+
+
+// const ContentStyle = styled('div')(({ theme }) => ({
+//     overflow: 'hidden',
+//     position: 'relative',
+//     backgroundColor: theme.palette.background.default
+// }));
+
+
+
+// const Page = () => {
+//     return (
+//         <RootStyle title="title" id="move_top" >
+//             {/* //     <ContentStyle> */}
+//             <h1>OKOKO</h1>
+//             {/* //     </ContentStyle> */}
+//         </RootStyle >
+//     )
+// }
+
+// export default Page
