f9f77746a2a33ebb9e5bb352fa422482a081f9d9
Arigus Wahyu <ariguswahyu.np@gmail.com>
2022-10-28 13:39:40 +0700

    add vercel analityc

---
+++ src/analytics.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Analytics } from '@vercel/analytics/react';
+
+const AnalyticsWrapper = () => {
+    return (
+        <Analytics />
+    )
+}
+
+// export function AnalyticsWrapper() {
+//     return <Analytics />;
+//   }
+
+export default AnalyticsWrapper
